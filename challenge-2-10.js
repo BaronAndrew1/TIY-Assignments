1a60e8d570ccffc269d57a80a8fa1f77c331f899
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-10 10:41:40 -0500

    add second challenge for object creation

--- challenge-2-10.js
+++ challenge-2-10.js
@@ -2,3 +2,15 @@ var x = input:"what is your birth year?";
 var y = input:"what is the current year?";
 input(parseInt());
 function calculateAge (x, y)
+
+//Second Challenge
+
+var marsala = new Object()
+    marsala.title = "chicken marsala";
+    marsala.servings = 4;
+    marsala.ingredients = ["chicken","marsala wine","chicken broth","mushrooms","garlic","basil","thyme","vegetable oil"];
+
+
+console.log("The title is " + marsala.title);
+console.log("The number of servings this recipe makes is " + marsala.servings);
+console.log("The ingredients are: " + marsala.ingredients);
