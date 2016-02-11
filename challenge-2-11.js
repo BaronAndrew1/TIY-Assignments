b47c160fcee10868d5a6e543711f6bee4a01585c
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-11 10:07:43 -0500

    minor edits to challenge 2-11

--- challenge-2-11.js
+++ challenge-2-11.js
@@ -1,3 +1,4 @@
+var x = "100";
 function lifetimeSupply(age, amountPerDay) {
 var maxAge = 100;
 var consumed = (100-age) * amountPerDay;
@@ -6,3 +7,4 @@ return consumed;
 console.log(lifetimeSupply(20, 5));
 console.log(lifetimeSupply(15, 10));
 console.log(lifetimeSupply(40, 2));
+window.alert("You will need " + (lifetimeSupply(20, 5)) + " to last until the ripe old age of " + x)
