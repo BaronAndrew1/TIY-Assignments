8a15efe9fba4c71c7eabae572abdd3e7a65d936c
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-11 10:55:53 -0500

    add second challenge to challenge-2-11

--- challenge-2-11.js
+++ challenge-2-11.js
@@ -8,3 +8,15 @@ console.log(lifetimeSupply(20, 5));
 console.log(lifetimeSupply(15, 10));
 console.log(lifetimeSupply(40, 2));
 window.alert("You will need " + (lifetimeSupply(20, 5)) + " to last until the ripe old age of " + x)
+
+
+
+var favoriteMovie = {
+    title: "potter",
+    duration: 2,
+    stars: ["someone", "someone else", "third person"],
+};
+function moviePrint() {
+    console.log(favoriteMovie.title + " lasts for " + favoriteMovie.duration + " minutes." + " Stars: " + favoriteMovie.stars[0],favoriteMovie.stars[1],favoriteMovie.stars[2] + ".");
+}
+moviePrint()
