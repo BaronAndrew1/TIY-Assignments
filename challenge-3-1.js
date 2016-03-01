bcbac01ea34391f5783027c201679ab1c183ecd3
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-01 10:40:20 -0500

    add challenge-3-1

---
+++ challenge-3-1.js
@@ -0,0 +1,5 @@
+function tagNum(tagType){
+  for(var i = 0; i < tagType.length; i++){     x = i;
+    console.log("There are " + x + " tags of type " + tagType + " on the page")
+}}
+tagNum($('p'));
