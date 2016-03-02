c0236cd522d5305cce0b9a15b9ef740512e3e4fe
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-02 10:34:12 -0500

    better functionality on challenge-3-2

--- challenge-3-2.js
+++ challenge-3-2.js
@@ -1,8 +1,11 @@
 for(var i = 0; i<21045; i++){
-  if(i % 2 === 0){
+  if(i === 0){
+    console.log("0 is not even or odd");
+  }
+  else if(i % 2 === 0){
     console.log(i + " is even");
   }
-  if(i % 2 === 1){
+  else if(i % 2 === 1){
     console.log(i + " is odd");
   }
   else{}
