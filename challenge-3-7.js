f477a1384755d66c7df46cdc3860ed8dbe932565
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-07 10:38:37 -0500

    add challenge-3-7

---
+++ challenge-3-7.js
@@ -0,0 +1,10 @@
+// 100% not working in the least
+
+var space = " ";
+var str = "floofloo flam fling"
+function letterCapitalize(string, separator) {
+  str.split(space);
+  str.charAt(0).toUpperCase();
+}
+
+console.log(letterCapitalize(str, space));
