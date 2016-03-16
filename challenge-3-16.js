9850ecefc0f11c0a4e5d5d1598831b33bbfa0e3d
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-16 10:28:31 -0400

    add second challenge to challenge-3-16

--- challenge-3-16.js
+++ challenge-3-16.js
@@ -6,3 +6,16 @@ function alphaSort(str){
 }
 alphaSort('happydog');
 alphaSort('webmaster');
+
+
+
+// CHALLENGE 2!!!
+
+function longestCountry(str){
+  var countries = str.split(", ");
+  countries.sort(function(a,b){
+   return a.length<b.length;
+  });
+  console.log("The longest country name is " + countries[0]);
+}
+longestCountry("United States of America, Australia, Germany, Dominican Republic");
