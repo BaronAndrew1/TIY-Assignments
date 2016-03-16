8412d9c52ae9d05c5d7094f3d95050a21912d6c3
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-16 09:18:08 -0400

    challenge-3-16 mod?? I am not sure what I changed

--- challenge-3-16.js
+++ challenge-3-16.js
@@ -1,7 +1,8 @@
 function alphaSort(str){
-  var splitString = str.split("");
+  var splitString = str.toLowerCase().split("");
   splitString.sort();
   var reString = splitString.join("");
   console.log(reString);
 }
 alphaSort('happydog');
+alphaSort('webmaster');
